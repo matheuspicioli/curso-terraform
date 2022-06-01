228de8a938f6719038eca58c7d2c4d80de696be9
Matheus Picioli <matheus.picioli98@gmail.com>
2022-05-31 23:23:14 -0300

    3 EC2 instances with security group for SSH access

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+    count = 3
+    ami = "ami-09d56f8956ab235b3"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+        Name = "dev${count.index}"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+    name = "acesso-ssh"
+    description = "acesso-ssh"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ssh"
+    }
+}
+
+resource "aws_s3_bucket" "dev4" {
+    bucket = "development-dev4"
+    acl = "private"
+
+    tags = {
+        Name = "development-dev4"
+    }
+}
